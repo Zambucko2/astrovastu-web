0ac0f28b5d92f604669c8c00ee4e61854519e91d
Zambucko2 <tattvam.vaidya@gmail.com>
2026-03-04 15:33:31 -0500

    Testing workflow trigger and relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/astrovastu-web/',
-  build: {
-    outDir: './dist',
-  },
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: './', // Using './' is a "magic fix" that makes paths relative to the folder
+})
